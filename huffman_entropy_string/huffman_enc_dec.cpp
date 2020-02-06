800d22032fdebba920a30e99bd1b4c856a3e3284
Karn <karn.gyan@criodo.com>
2020-02-06 14:43:47 +0530

    pre 6th feb

---
+++ huffman_entropy_string/huffman_enc_dec.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h> 
+#define MAX_TREE_HT 256 
+using namespace std; 
+
+map<char, string> codes; 
+
+map<char, int> freq;
+
+map<char, long double> prob;
+
+struct MinHeapNode { 
+	char data;			   
+	int freq;			  
+	MinHeapNode *left, *right;  
+
+	MinHeapNode(char data, int freq) 
+	{ 
+		left = right = NULL; 
+		this->data = data; 
+		this->freq = freq; 
+	} 
+}; 
+
+struct compare { 
+	bool operator()(MinHeapNode* l, MinHeapNode* r) 
+	{ 
+		return (l->freq > r->freq); 
+	} 
+}; 
+
+void printCodes(struct MinHeapNode* root, string str) { 
+	if (!root) 
+		return; 
+	if (root->data != '$') 
+		cout << root->data << ": " << str << "\n"; 
+	printCodes(root->left, str + "0"); 
+	printCodes(root->right, str + "1"); 
+} 
+
+void storeCodes(struct MinHeapNode* root, string str) { 
+	if (root==NULL) 
+		return; 
+	if (root->data != '$') 
+		codes[root->data]=str; 
+	storeCodes(root->left, str + "0"); 
+	storeCodes(root->right, str + "1"); 
+} 
+
+priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap; 
+
+void HuffmanCodes(int size) { 
+	
+    struct MinHeapNode *left, *right, *top; 
+	
+    for (map<char, int>::iterator v=freq.begin(); v!=freq.end(); v++) 
+		minHeap.push(new MinHeapNode(v->first, v->second)); 
+
+	while (minHeap.size() != 1) { 
+		left = minHeap.top(); 
+		minHeap.pop(); 
+		right = minHeap.top(); 
+		minHeap.pop(); 
+		top = new MinHeapNode('$', left->freq + right->freq); 
+		top->left = left; 
+		top->right = right; 
+		minHeap.push(top); 
+	} 
+	
+    storeCodes(minHeap.top(), ""); 
+} 
+
+void calcFreq(string str, int n) { 
+	for (int i=0; i<str.size(); i++) 
+		freq[str[i]]++; 
+} 
+
+string decode_file(struct MinHeapNode* root, string s) {
+
+	string ans = ""; 
+	
+    struct MinHeapNode* curr = root;
+
+	for (int i=0;i<s.size();i++) { 
+		if (s[i] == '0') 
+		curr = curr->left; 
+		else
+		curr = curr->right; 
+
+		if (curr->left==NULL and curr->right==NULL) 
+		{ 
+			ans += curr->data; 
+			curr = root; 
+		} 
+	}
+
+	return ans+'\0'; 
+} 
+
+long double calc_entropy() {
+    long double H = 0.0;
+
+    for (auto it : prob) {
+        long double pi = it.second;
+        long double contrib = -pi * log2(pi);
+        H += contrib;
+    }
+
+    return H;
+}
+
+int main() {
+
+	freopen("in", "r", stdin);
+    
+    string str;
+    
+    cin >> str;
+    
+    int size = str.length();
+
+	string encodedString, decodedString; 
+	
+    calcFreq(str, size); 
+	
+    HuffmanCodes(size); 
+	
+    cout << "Huffman Codes of characters in the input string:\n"; 
+	
+    for (auto v : codes) 
+		cout << v.first <<' ' << v.second << endl; 
+
+	for (auto i: str)
+		encodedString+=codes[i]; 
+
+	cout << "\nEncoded Huffman data:\n" << encodedString << endl; 
+
+	decodedString = decode_file(minHeap.top(), encodedString); 
+
+	cout << "\nDecoded Huffman Data:\n" << decodedString << endl; 
+        
+    for (auto v: freq) {
+        long double x = (long double)v.second / (long double) size;
+        prob[v.first] = x;
+    }
+
+    long double H = calc_entropy();
+    cout << "\nTotal Entropy:\n" << H << endl;
+
+    return 0; 
+}
+
---
+++ lcs/lcsstring.cpp
@@ -0,0 +1,81 @@
+/*
+
+Author: @karngyan
+Team: BlundersPride
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef std::vector<ll> vll;
+
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define endl '\n'
+#define pb push_back
+#define rep(i,a,b) for(ll i=a;i<b;++i)
+#define rrep(i,a,b) for(ll i=a;i>b;--i)
+#define cres(c,n) (c).clear(),(c).resize(n)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define ff first
+#define ss second
+#define mk make_pair
+string s , t;
+ll ns , nt ;
+ll dp[3010][3010];
+
+ll go(ll i , ll j)
+{
+	if(i >= ns or j >= nt)
+		return 0;
+	ll &ans = dp[i][j];
+	if(ans != -1)
+		return ans;
+	if(s[i] == t[j])
+		ans = 1 + go(i+1 , j+1);
+	else
+		ans = max(go(i , j+1) , go(i+1 , j));
+
+	return ans;
+}
+
+
+
+signed main()
+{
+	fast;
+	memset(dp , -1 , sizeof dp);
+	
+	cin >> s >> t;
+	ns = s.length();
+	nt = t.length();
+	ll ans = go(0,0);
+	// cout<<go(0,0)<<endl;
+
+	ll i = 0, j = 0;
+	while(i<ns and j<nt)
+	{
+		if(s[i] == t[j])
+		{
+			cout<<s[i];
+			i++, j++;
+		}
+		else
+		{
+			if(dp[i+1][j] > dp[i][j+1])
+				i++;
+			else
+				j++;
+		}
+	}
+
+
+    cout << endl;
+
+	
+	return 0;
+}
+
